4487818a755a7d8a8c79c69f1b2efb9ce28064d7
Egor Shmelev <shmelevyegor25@gmail.com>
2024-07-16 15:51:03 -0300

    added  models User and Habit

---
+++ habits/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+from users.models import User
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Habit(models.Model):
+    FREQUENCY_UNITS = [
+        ("minutes", "минуты"),
+        ("hours", "часы"),
+        ("days", "дни"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='создатель привычки')
+    place = models.CharField(max_length=200, verbose_name='место привычки')
+    time = models.DateTimeField(verbose_name='время привычки')
+    action = models.CharField(max_length=200, verbose_name='действие')
+    is_pleasant = models.BooleanField(verbose_name='приятная', **NULLABLE)
+    related_habit = models.ForeignKey('self', on_delete=models.SET_NULL, **NULLABLE)
+    frequency_number = models.PositiveIntegerField()
+    frequency_unit = models.CharField(max_length=10, choices=FREQUENCY_UNITS, default='days')
+    reward = models.CharField(max_length=200, verbose_name='Вознаграждение', **NULLABLE)
+    duration = models.DurationField()
+    is_public = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.user} - {self.action}'
+
+    class Meta:
+        verbose_name = 'привычка'
+        verbose_name_plural = 'привычки'
--- users/models.py
+++ users/models.py
@@ -1,5 +1,20 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватарка', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
