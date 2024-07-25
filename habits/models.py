ca722cf6a328792e5072bb3431e56a9a41d4d18c
Egor Shmelev <shmelevyegor25@gmail.com>
2024-07-24 23:15:13 -0300

    added documentation

--- habits/models.py
+++ habits/models.py
@@ -19,12 +19,13 @@ class Habit(models.Model):
     time = models.DateTimeField(verbose_name='время привычки')
     action = models.CharField(max_length=200, verbose_name='действие')
     is_pleasant = models.BooleanField(verbose_name='приятная')
-    related_habit = models.ForeignKey('self', on_delete=models.SET_NULL, **NULLABLE)
-    frequency_number = models.PositiveIntegerField()
-    frequency_unit = models.CharField(max_length=10, choices=FREQUENCY_UNITS, default='days')
+    related_habit = models.ForeignKey('self', on_delete=models.SET_NULL, **NULLABLE, verbose_name='связанная привычка')
+    frequency_number = models.PositiveIntegerField(verbose_name='количество раз')
+    frequency_unit = models.CharField(max_length=10, choices=FREQUENCY_UNITS, default='days',
+                                      verbose_name='единицы измерения')
     reward = models.CharField(max_length=200, verbose_name='Вознаграждение', **NULLABLE)
-    duration = models.DurationField()
-    is_public = models.BooleanField()
+    duration = models.DurationField(verbose_name='длительность действия')
+    is_public = models.BooleanField(verbose_name='публичная')
 
     def __str__(self):
         return f'{self.user} - {self.action}'
